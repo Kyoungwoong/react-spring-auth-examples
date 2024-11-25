a9910d02b52f3cbb9d86b6b266e17ad80391d358
Kyoungwoong <kyy980708@gmail.com>
2024-11-25 11:23:49 +0900

    feat: frontend Implement cookie-based login/logout functionality

--- frontend/src/interface/RoutePath.ts
+++ frontend/src/interface/RoutePath.ts
@@ -1,3 +1,4 @@
 export enum RoutePath {
     HOMEPAGE = "/",
+    COOKIE = "/login-cookie"
 }
--- frontend/src/pages/AppRoutes.tsx
+++ frontend/src/pages/AppRoutes.tsx
@@ -1,12 +1,14 @@
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import { RoutePath } from '../interface/RoutePath';
 import HomePage from './home/HomePage';
+import Cookie from './cookie/Cookie';
 
 const AppRoutes = () => {
     return (
       <Router>
           <Routes>
               <Route path={RoutePath.HOMEPAGE} element={<HomePage />} />
+              <Route path={RoutePath.COOKIE} element={<Cookie />} />
           </Routes>
       </Router>
     )
